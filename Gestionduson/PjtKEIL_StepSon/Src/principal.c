bda521c247c627f3a244b1c64e742cb0bced60ba
Clotilde Berthelemy <berthele@insat.insa-toulouse.fr>
2021-05-06 17:51:23 +0200

    Callbackson + configuration PWM

--- Gestionduson/PjtKEIL_StepSon/Src/principal.c
+++ Gestionduson/PjtKEIL_StepSon/Src/principal.c
@@ -14,15 +14,19 @@ int main(void)
 // Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
 CLOCK_Configure();
 
-// configuration du Timer 4 en débordement 100ms
+// configuration de CallbackSon sur Timer 4 en débordement 100ms
 	Timer_1234_Init_ff(TIM4, 6552);
+	Active_IT_Debordement_Timer(TIM4,2,callbackson);
 
 // Activation des interruptions issues du Timer 4
 // Association de la fonction à exécuter lors de l'interruption : timer_callback
 // cette fonction (si écrite en ASM) doit être conforme à l'AAPCS
 	
 //** Placez votre code là ** // 	
-	Active_IT_Debordement_Timer(TIM4,2,callbackson);
+	
+//configuration de la PWM Timer 3 canal 3 sur PB.0
+	PWM_Init_ff(TIM3,3,720);
+	GPIO_Configure(GPIOB,0,OUTPUT,ALT_PPULL);
 	
 	
 
