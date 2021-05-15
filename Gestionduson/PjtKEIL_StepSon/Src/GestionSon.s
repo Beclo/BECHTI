b46bf0241051781d8a051673c65e557bf7dd4c9f
Clotilde Berthelemy <berthele@insat.insa-toulouse.fr>
2021-05-15 18:32:46 +0200

    Finalisation de StepSon : codage de startson + résolution pb index

--- Gestionduson/PjtKEIL_StepSon/Src/GestionSon.s
+++ Gestionduson/PjtKEIL_StepSon/Src/GestionSon.s
@@ -28,18 +28,24 @@ Index dcd 0
 	; Import du fichier DriverJeuLaser.inc
 	include ./Driver/DriverJeuLaser.inc
 
+startson proc
+	mov R5,#0
+	strh R5,[R2]
+	
+	endp
 	
 callbackson proc
-	push{lr,R4-R7}
+	push{lr,R4-R8}
 	
 	ldr R0,=SortieSon
 	ldr R1,=Son
 	ldr R2,=Index
 	ldrsh R5, [R2]
 	ldr R4,=LongueurSon
+	ldrsh R8, [R4]
 
-	cmp R5,R4 
-	beq FinSi ;If Index==LongueurSon
+	cmp R5,R8
+	beq Sinon ;If Index==LongueurSon
 
 ;If Index!=LongueurSon
 Alors
@@ -56,15 +62,22 @@ Alors
 	add R5,#1 ;Index++
 	strh R5,[R2] ;mise à jour valeur Index
 	
-	mov R0,#360
+	mov R0,R3
 	bl PWM_Set_Value_TIM3_Ch3 ;appel de la fct PWM_Set_Value_TIM3_Ch3 pour mettre à jour rapport cyclique
 	
 	b FinSi
 	
+Sinon
+	bl startson
+	b FinSi
+	
+	
 ; If Index >=LongueurSon
 FinSi
-	pop{lr,R4-R7}
+	pop{lr,R4-R8}
 	bx lr
 	endp
+		
+	
 		
 	END	
